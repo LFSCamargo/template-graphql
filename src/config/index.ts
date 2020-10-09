3152eab2f11e76a674e81771129e131620915ff2
Luiz Fernando <luizepauloxd@gmail.com>
2020-10-09 20:33:01 -0300

    fix: db name

--- src/config/index.ts
+++ src/config/index.ts
@@ -5,5 +5,5 @@ dotEnv.config();
 export const JWT = process.env.JWT || "topsecret";
 export const PORT = process.env.PORT || 5000;
 export const MONGO_URL =
-  process.env.MONGO_URL || "mongodb://localhost/remindme";
+  process.env.MONGO_URL || "mongodb://localhost/template-graphql";
 export const ENV = process.env.ENV || "DEV";
